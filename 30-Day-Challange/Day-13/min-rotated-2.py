8586932474ad4f418fbe49308cc5ddff2c7e94cc
EashanKaushik <EashanK16@gmail.com>
2021-06-17 10:15:36 +0530

    17-06-2021

--- 30-Day-Challange/Day-13/min-rotated-2.py
+++ 30-Day-Challange/Day-13/min-rotated-2.py
@@ -5,11 +5,14 @@ class Solution:
         right = len(nums) - 1
 
         while left < right:
-
             mid = left + (right - left)//2
 
             if mid > 0 and nums[mid] < nums[mid - 1]:
                 return nums[mid]
+
+            if nums[mid] == nums[left] and nums[mid] == nums[right]:
+                left += 1
+                continue
 
             if nums[mid] >= nums[left] and nums[mid] > nums[right]:
                 left = mid + 1
@@ -19,6 +22,7 @@ class Solution:
                 break
 
         return nums[left]
+
 
 
 s = Solution()
